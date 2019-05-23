ef4714bb8adffdddad0cee3f5b79538e774a6f3a
Daniel <rodriguezdl@vcu.edu>
2019-05-23 01:27:59 -0400

    First Commit onto music_app repository

---
+++ app/src/main/java/com/example/daniel/music_player/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.daniel.music_player;
+
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button play, prev, next;
+    int pausePosition; //to figure out the pause position when track is playing
+    private ArrayList<Song> songList;
+    private ListView songView;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = findViewById(R.id.play_button);
+        prev = findViewById(R.id.previous_button);
+        next = findViewById(R.id.next_button);
+        songView = findViewById(R.id.song_list);
+
+        songList = new ArrayList<Song>();
+        getSongList();
+        Collections.sort(songList, new Comparator<Song>(){
+            public int compare(Song a, Song b){
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void getSongList(){
+        ContentResolver musicResolver = getContentResolver(); //grabs the music files from the device
+        Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI; //grabs the actual music file info
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null);
+
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            int titleColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+
+            do{
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while(musicCursor.moveToNext());
+        }
+    }
+}
